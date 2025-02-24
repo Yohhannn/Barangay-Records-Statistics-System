6f768892aa1ef80b648544c28928ea493a6d548e
rexshimura <mortpauljpm03@gmail.com>
2025-02-25 03:48:34 +0800

    + Updated UI Components + Added Assets Folder + Modified the UI Component Properties -- such as QObject Names and Stylesheets
    -JP

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import sys
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtGui
 from PySide6.QtUiTools import QUiLoader
 
 loader = QUiLoader()
 
 app = QtWidgets.QApplication(sys.argv)
-window = loader.load("UI/test_login_v2.ui", None)
+window = loader.load("UI/login.ui", None)
 
 def login_Button() :
     print("-- Login Attempt")
-    print("Employee ID:", window.line_edit_emp_id.text(), " PIN:", window.line_edit_emp_pin.text())
+    print("Employee ID:", window.login_fieldEmp_id.text(), " PIN:", window.login_fieldPin.text())
 
-
-
+# Manually Refer the component named login_imageLogo to the logo
+window.login_imageLogo.setPixmap(QtGui.QPixmap("Assets/logo_brgy.png"))
+window.setWindowIcon(QtGui.QIcon("Assets/icon_main.png"))
 window.setWindowTitle("Marigondon Barangay Profiling System")
 
-window.push_button_login.clicked.connect(login_Button)
+window.login_buttonLogin.clicked.connect(login_Button)
 window.show()
 app.exec()
