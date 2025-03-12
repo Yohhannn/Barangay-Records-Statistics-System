f1481502e9fca7f1eab8332d9cc5d0e103365b8b
Yohhan <53113613+Yohhannn@users.noreply.github.com>
2025-03-12 19:49:22 +0800

    Resized and added seconds

--- Utils/utils_realtime.py
+++ Utils/utils_realtime.py
@@ -2,7 +2,7 @@ from PySide6.QtCore import QTimer
 from datetime import datetime
 
 def update_time_label(label):
-    """Update the label with the current time."""
+    """Update the label with the current time, including seconds."""
     now = datetime.now()
-    formatted_time = now.strftime("%I:%M %p")  # Format as "10:02 PM"
+    formatted_time = now.strftime("%I:%M:%S %p")  # Format as EX: "10:02:45 PM"
     label.setText(formatted_time)
