5632d4bdd701ad6a4718265fb9d2c1856ce46ee8
Yohhannn <joehanes.lauglaug@ctu.edu.ph>
2025-05-16 02:53:13 +0800

    - Added date and time on statistics - Optimized loading system for date and time

--- Controllers/UserController/Statistics/Demographics/demographics_func.py
+++ Controllers/UserController/Statistics/Demographics/demographics_func.py
@@ -1,4 +1,6 @@
 from PySide6.QtGui import QIcon
+from PySide6.QtCore import QTimer
+from PyQt6.QtCore import QDateTime
 from Controllers.BaseFileController import BaseFileController
 
 class demographics_func(BaseFileController):
@@ -6,6 +8,8 @@ class demographics_func(BaseFileController):
         super().__init__(login_window, emp_first_name)
         self.stack = stack
         self.stat_demo_screen = self.load_ui("Resources/UIs/MainPages/StatisticPages/demographic.ui")
+        self._setup_date()
+        self._setup_time()
         self.setup_demo_ui()
         self.center_on_screen()
 
@@ -23,6 +27,29 @@ class demographics_func(BaseFileController):
         # Return Button
         self.stat_demo_screen.btn_returnToStatisticsPage.clicked.connect(self.goto_statistics_panel)
 
+    def _setup_time(self):
+        self.timer = QTimer(self.stat_demo_screen)
+
+        # Initialize with current date/time
+        self._setup_date()
+
+        # Connect timer to update function
+        self.timer.timeout.connect(self._setup_date)
+        self.timer.start(1000)  # Update every second
+
+    def _setup_date(self):
+        """Update both date and time labels"""
+        current_datetime = QDateTime.currentDateTime()
+
+        # Update date label
+        formatted_date = current_datetime.toString("MMMM d, yyyy")
+        day_of_week = current_datetime.toString("dddd")
+        self.stat_demo_screen.text_date.setText(f"{formatted_date} - {day_of_week}")
+
+        # Update time label
+        formatted_time = current_datetime.toString("h:mm:ss AP")
+        self.stat_demo_screen.text_time.setText(formatted_time)
+
     def goto_statistics_panel(self):
         """Handle navigation to Statistics Panel screen."""
         print("-- Navigating to Statistics")
@@ -33,3 +60,5 @@ class demographics_func(BaseFileController):
 
         self.stack.setCurrentWidget(self.statistics_panel.statistics_screen)
         self.setWindowTitle("MaPro: Statistics")
+
+
