00eae0d6072751215470bc8da937ce1db273f9fb
Yohhannn <joehanes.lauglaug@ctu.edu.ph>
2025-05-16 02:55:05 +0800

    - 4.2.9 - Pre Alpha

--- Views/DashboardView.py
+++ Views/DashboardView.py
@@ -14,7 +14,7 @@ class DashboardView:
         self.dashboard_screen = None
 
         self.app_name = "MaPro"
-        self.app_version = "4.2.3 - Pre Alpha"
+        self.app_version = "4.2.9 - Pre Alpha"
 
     def setup_dashboard_ui(self, ui_screen):
         self.dashboard_screen = ui_screen
