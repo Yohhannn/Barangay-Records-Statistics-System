5d2c042aa424519937efdcc831f91973d78b6ced
Yohhan <53113613+Yohhannn@users.noreply.github.com>
2025-06-08 14:18:02 +0800

    Revert "QUERY UPDATE"

--- Models/Statistics/InfrastructureModel.py
+++ Models/Statistics/InfrastructureModel.py
@@ -2,8 +2,8 @@ from database import Database
 
 class InfrastructureModel:
     def __init__(self):
-        self.connection = Database()
-        self.cursor = self.connection.get_cursor()
+        self.db = Database()
+        self.cursor = self.db.get_cursor()
 
     def get_total_sitio_infrastructure(self, from_date, to_date):
         try:
