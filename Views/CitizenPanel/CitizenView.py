0bbd2bccac0f7760b19ab5bdbfb4c361f88ae798
Yohhan <53113613+Yohhannn@users.noreply.github.com>
2025-06-09 21:54:04 +0800

    Partial progress for citizen update

--- Views/CitizenPanel/CitizenView.py
+++ Views/CitizenPanel/CitizenView.py
@@ -164,6 +164,150 @@ class CitizenView:
 
         return self.part3_popup
 
+    def show_update_citizen_part_01_popup(self, parent):
+        self.part1_popup_update = load_popup(
+            "Resources/UIs/PopUp/Screen_CitizenPanel/ScreenCitizenProfile/Update/edit_register_citizen_part_01.ui", parent)
+        # self.stack.addWidget(self.part1_popup)
+        self.part1_popup_update.setWindowTitle("Register New Citizen")
+        self.part1_popup_update.setWindowModality(Qt.ApplicationModal)
+        self.part1_popup_update.setFixedSize(self.part1_popup_update.size())
+        self.part1_popup_update.register_buttonPrev.setIcon(QIcon('Resources/Icons/FuncIcons/icon_arrow_prev'))
+        self.part1_popup_update.register_buttonConfirmPart1_NextToPart2.setIcon(
+            QIcon('Resources/Icons/FuncIcons/icon_arrow_next'))
+        self.part1_popup_update.register_buttonConfirmPart1_NextToPart2.clicked.connect(self.controller.update_validate_part1_fields)
+
+        try:
+            db = Database()
+            cursor = db.get_cursor()
+            cursor.execute("SELECT rel_id, rel_name FROM religion ORDER BY rel_name ASC;")
+            results = cursor.fetchall()
+
+            combo = self.part1_popup_update.register_citizen_comboBox_Religion
+            combo.clear()
+            for rel_id, rel_name in results:
+                combo.addItem(rel_name, rel_id)
+
+        except Exception as e:
+            print(f"Failed to load religion : {e}")
+        finally:
+            db.close()
+        # self.setup_image_handlers(self.part1_popup)
+        # if hasattr(self, 'citizen_data'):
+        #     self.restore_part1_data()
+        # self.part1_popup.exec_()
+
+        return self.part1_popup_update
+
+    def show_update_citizen_part_02_popup(self, parent):
+        # part_one_popup.close()
+        self.part2_popup_update = load_popup(
+            "Resources/UIs/PopUp/Screen_CitizenPanel/ScreenCitizenProfile/Update/edit_register_citizen_part_02.ui", parent)
+        self.part2_popup_update.setWindowTitle("Register New Citizen - Part 2")
+        self.part2_popup_update.setWindowModality(Qt.ApplicationModal)
+        self.part2_popup_update.setFixedSize(self.part2_popup_update.size())
+        self.part2_popup_update.register_buttonReturnToPart1_FromPart2.setIcon(
+            QIcon('Resources/Icons/FuncIcons/icon_arrow_prev'))
+        self.part2_popup_update.register_buttonConfirmPart2_NextToPart3.setIcon(
+            QIcon('Resources/Icons/FuncIcons/icon_arrow_next'))
+        self.part2_popup_update.register_buttonConfirmPart2_NextToPart3.clicked.connect(self.controller.update_validate_part2_fields)
+        self.part2_popup_update.register_buttonReturnToPart1_FromPart2.clicked.connect(
+            self.controller.update_return_to_part1_from_part2)
+        try:
+            db = Database()
+            cursor = db.get_cursor()
+            cursor.execute("SELECT es_id, es_status_name FROM employment_status ORDER BY es_status_name ASC;")
+            results = cursor.fetchall()
+
+            combo = self.part2_popup_update.register_citizen_comboBox_EmploymentStatus
+            combo.clear()
+            for es_id, es_status_name in results:
+                combo.addItem(es_status_name, es_id)
+
+        except Exception as e:
+            print(f"Failed to load employment status: {e}")
+        finally:
+            db.close()
+
+        try:
+            db = Database()
+            cursor = db.get_cursor()
+            cursor.execute("SELECT pc_id, pc_category_name FROM philhealth_category ORDER BY pc_category_name ASC;")
+            results = cursor.fetchall()
+
+            combo = self.part2_popup_update.register_citizen_comboBox_PhilCat
+            combo.clear()
+            for pc_id, pc_category_name in results:
+                combo.addItem(pc_category_name, pc_id)
+
+        except Exception as e:
+            print(f"Failed to load philhealth categopry name: {e}")
+        finally:
+            db.close()
+        # self.part2_popup_update.exec_()
+
+        try:
+            db = Database()
+            cursor = db.get_cursor()
+            cursor.execute("SELECT pc_id, pc_category_name FROM philhealth_category ORDER BY pc_category_name ASC;")
+            results = cursor.fetchall()
+
+            combo = self.part2_popup_update.register_citizen_comboBox_PhilCat
+            combo.clear()
+            for pc_id, pc_category_name in results:
+                combo.addItem(pc_category_name, pc_id)
+
+        except Exception as e:
+            print(f"Failed to load philhealth categopry name: {e}")
+        finally:
+            db.close()
+
+        return self.part2_popup_update
+        # return_to_part1_from_part2
+
+    def show_update_citizen_part_03_popup(self, parent):
+        # part_two_popup.close()
+        self.part3_popup_update = load_popup(
+            "Resources/UIs/PopUp/Screen_CitizenPanel/ScreenCitizenProfile/Update/edit_register_citizen_part_03.ui", parent)
+        self.part3_popup_update.setWindowTitle("Register New Citizen - Part 3")
+        self.part3_popup_update.setWindowModality(Qt.ApplicationModal)
+        self.part3_popup_update.setFixedSize(self.part3_popup_update.size())
+        self.part3_popup_update.register_buttonReturnToPart2_FromPart3.setIcon(
+            QIcon('Resources/Icons/FuncIcons/icon_arrow_prev'))
+        self.part3_popup_update.register_buttonConfirmPart3_SaveForm.setIcon(QIcon('Resources/Icons/FuncIcons/icon_confirm'))
+        self.part3_popup_update.register_buttonConfirmPart3_SaveForm.clicked.connect(self.controller.update_validate_part3_fields)
+        self.part3_popup_update.register_buttonReturnToPart2_FromPart3.clicked.connect(
+            self.controller.update_return_to_part2_from_part3)
+
+        # if hasattr(self, 'citizen_data'):
+        #     self.restore_part3_data()
+        # self.setup_radio_button_groups_03()
+        # save_btn = self.part3_popup_update.findChild(QPushButton, "register_buttonConfirmPart3_SaveForm")
+        # if save_btn:
+        #     save_btn.clicked.connect(self.confirm_and_save)
+        # back_btn = self.part3_popup_update.findChild(QPushButton, "register_buttonReturnToPart2_FromPart3")
+        # if back_btn:
+        #     back_btn.clicked.connect(self.return_to_part2_from_part3)
+        # self.part3_popup_update.exec_()
+
+        try:
+            db = Database()
+            cursor = db.get_cursor()
+            cursor.execute("SELECT edat_id, edat_level FROM EDUCATIONAL_ATTAINMENT ORDER BY edat_level ASC;")
+            results = cursor.fetchall()
+
+            combo = self.part3_popup_update.register_citizen_comboBox_EducationalLevel
+            combo.clear()
+            for edat_id, educational_attainment in results:
+                combo.addItem(educational_attainment, edat_id)
+
+        except Exception as e:
+            print(f"Failed to load educ atainment: {e}")
+        finally:
+            db.close()
+
+        return self.part3_popup_update
+
+
     def setup_profile_ui(self, ui_screen):
         self.cp_profile_screen = ui_screen
         ui_screen.setFixedSize(1350, 850)
@@ -177,6 +321,7 @@ class CitizenView:
         # ui_screen.profileList_buttonFilter.setIcon(QIcon('Resources/Icons/FuncIcons/icon_filter.svg'))
         ui_screen.btn_returnToCitizenPanelPage.clicked.connect(self.controller.goto_citizen_panel)
         ui_screen.cp_citizen_button_register.clicked.connect(self.controller.show_register_citizen_part_01_initialize)
+        ui_screen.cp_citizen_button_update.clicked.connect(self.controller.show_update_citizen_part_01_initialize)
         ui_screen.cp_tableView_List_RegCitizens.cellClicked.connect(self.controller.handle_row_click_citizen)
         ui_screen.cp_CitizenName_buttonSearch.clicked.connect(self.controller.perform_citizen_search)
         ui_screen.cp_citizen_button_remove.clicked.connect(self.controller.handle_remove_citizen)
